4b4ed52fb33196a3a8935daa0a08e2d83a1cc8dd
Matt Goldman <hello@mattgoldman.biz>
2012-03-21 08:14:20 -0700

    responsive template imported, needs more testing

---
+++ responsive/config.rb
@@ -0,0 +1,17 @@
+# File system locations
+css_dir               = File.join 'css'
+sass_dir              = File.join 'scss'
+
+# Syntax preference
+preferred_syntax      = :scss
+
+# Determine whether Compass generates relative or absolute paths
+relative_assets       = false
+
+# Determines whether line comments should be added to compiled css for easier debugging
+line_comments         = false
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style          = :compressed
+
+# Learn more: http://beta.compass-style.org/help/tutorials/configuration-reference/
